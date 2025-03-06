0fef75b1cab604fe00b6ec060cdb08567bc68112
Зенченко Евгений <zenchenko_es@outlook.com>
2025-03-07 00:05:21 +0700

    reject project

--- src/Game.jsx
+++ src/Game.jsx
@@ -14,19 +14,19 @@ const WIN_PATTERNS = [
 ];
 
 //выводит массив field (с помощью метода map());
-export const FieldLayout = () => (
+export const FieldLayout = ({ field }) => (
+	//const { field } = props;
+
 	<div className={styles.parent}>
 		<div className={styles.block}>
 			<div className={styles.grid}>
-				<div className={styles.cell}></div>
-				<div className={styles.cell}></div>
-				<div className={styles.cell}></div>
-				<div className={styles.cell}></div>
-				<div className={styles.cell}></div>
-				<div className={styles.cell}></div>
-				<div className={styles.cell}></div>
-				<div className={styles.cell}></div>
-				<div className={styles.cell}></div>
+				{field.map((num, index) => {
+					return (
+						<div key={index} className={styles.cell}>
+							{num}
+						</div>
+					);
+				})}
 			</div>
 		</div>
 	</div>
@@ -47,10 +47,131 @@ export const Game = () => {
 	const [currentPlayer, setCurrentPlayer] = useState('X'); //кто ходит в данный момент
 	const [isGameEnded, setIsGameEnded] = useState(false); //была ли завершена игра
 	const [isDraw, setIsDraw] = useState(false); //была ли ничья
-	const [field, setField] = useState(Array(9).fill(null)); //массив с 9 ячейками
+	const [field, setField] = useState(Array(9).fill(' ')); //массив с 9 ячейками
 
 	//['', '', '', '', '', '', '', '', ''];
-	return <FieldLayout />;
+	//return <FieldLayout field={setField(field)} />;
+	// const getClick = () => {
+	// 	console.log('click');
+	// };
+
+	// const handleClick = (index) => {
+	// 	if (field[index] || isGameEnded) {
+	// 		return;
+	// 	}
+
+	// 	const newField = [...field];
+	// 	newField[index] = currentPlayer;
+	// setCurrentPlayer(currentPlayer === "x" ? "o" : "x"); //меняем игрока
+	// };
+
+	// const Click = (e, index) => {
+	// 	// console.log('click');
+	// 	// console.log(e[index]);
+	// 	// if (field[index] || isGameEnded) {
+	// 	// 	return;
+	// 	// }
+
+	// 	// const newField = [...field];
+	// 	// newField[index] = currentPlayer;
+	// 	setCurrentPlayer(currentPlayer === 'x' ? 'o' : 'x'); //меняем игрока
+	// 	console.log(field);
+	// };
+	// const winner = WIN_PATTERNS.every((cellIndex) => {
+	// 	return field[cellIndex] === currentPlayer;
+	// 	//console.log(currentPlayer);
+
+	// 	//console.log(cellIndex[]);
+	// 	//console.log(currentPlayer);
+
+	// 	//field[cellIndex] === currentPlayer;
+	// });
+	// console.log(winner);
+	//console.log(WIN_PATTERNS[0][2]);
+
+	//let s = [];
+
+	const X = field
+		.map((cellIndex, index) => {
+			if (cellIndex === 'X') return index;
+		})
+		.filter((cellIndex) => cellIndex !== undefined);
+
+	const O = field
+		.map((cellIndex, index) => {
+			if (cellIndex === 'O') return index;
+		})
+		.filter((cellIndex) => cellIndex !== undefined);
+	// console.log('ddd', X);
+
+	// console.log();
+
+	const compareFunc = (a, b) =>
+		a.length === b.length && a.every((element, index) => element === b[index]);
+
+	// let a = X;
+	// let b = WIN_PATTERNS[0];
+	WIN_PATTERNS.forEach((cellIndex) => {
+		if (compareFunc(X, cellIndex)) {
+			console.log('X win');
+		}
+		if (compareFunc(O, cellIndex)) {
+			console.log('O win');
+		}
+	});
+	console.log(O);
+
+	//console.log(compareFunc(X, WIN_PATTERNS[0]));
+
+	// const s = X.every((element, index) => element !== WIN_PATTERNS[index]);
+
+	// console.log(s);
+
+	// //console.log(s);
+	// if (s === O) {
+
+	// }
+	// if (s === X) {
+	// 	console.log('X win');
+	// }
+
+	//console.log(X);
+
+	return (
+		<div className={styles.parent}>
+			<div className={styles.block}>
+				<div className={styles.grid}>
+					{field.map((num, index) => {
+						return (
+							<div
+								onClick={() => {
+									//const newField = [...field];
+									const newField = [...field];
+
+									newField[index] = currentPlayer;
+									setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+									setField(newField);
+
+									//console.log(newField);
+									//setField(num);
+									// console.log(num[currentPlayer]);
+
+									// setCurrentPlayer(
+									// 	currentPlayer[index] === 'x' ? 'o' : 'x',
+									// );
+									//console.log(currentPlayer[index]);
+								}}
+								key={index}
+								className={styles.cell}
+							>
+								{num}
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 //const cells = Array(9).fill(null); //массив с 9 ячейками
